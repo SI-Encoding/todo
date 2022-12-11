fa6070e3bd87b5a6db255e4ed21f38062520901b
SI-Encoding <stanscoding12+encoding@gmail.com>
2022-12-10 16:28:03 -0800

    Added form

--- src/components/MainTask.vue
+++ src/components/MainTask.vue
@@ -13,9 +13,45 @@
                 What do <span class="text-warning">you</span> want to do?
             </h1>
             <p class="lead my-4">Schedule a task</p>
-            <button class="btn btn-success"><i class="bi bi-plus"></i>Add a new task</button>
+            <button class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#addTaskModal"><i class="bi bi-plus"></i>Add a new task</button>
         </div>
     </section>
+    <!-- Modal -->
+<div class="modal fade" id="addTaskModal" tabindex="-1" role="dialog" aria-labelledby="addTaskModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addTaskModalLabel">Add a new task</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form  @submit.prevent="onSubmit">
+            <div class="form-group">
+                <label for="taskName">Enter the name of the new task:</label>
+                <input type="text" class="form-control" id="taskName" v-model="taskName" aria-describedby="taskNameHelp" placeholder="Enter task name" required>
+          
+       
+                <label for="taskNotes">Enter the notes of the new task:</label>
+                <input type="text" class="form-control" id="taskNotes" v-model="taskNote" aria-describedby="taskNotesHelp" placeholder="Enter task notes" required>
+       
+        
+                <label for="taskDate">Pick a date</label>
+                <input type="date" class="form-control" id="taskDate" v-model="taskDate" aria-describedby="taskDateHelp" placeholder="Choose a date" required>
+                <label for="taskTime">Pick a time</label>
+                <input type="time" class="form-control" id="taskTime" v-model="taskTime" aria-describedby="taskTimeHelp" placeholder="Choose a time" required>
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 </template>
 
 <script>
@@ -24,7 +60,19 @@ export default {
 //   props: {
 //     msg: String
 //   }
-name: 'MainTask'
+name: 'MainTask',
+data() {
+    return {
+        taskName: '',
+        taskNote: '',
+        taskDate: '',
+        taskTime: ''
+    
+    }
+},
+methods: {
+
+}
 }
 
 </script>
